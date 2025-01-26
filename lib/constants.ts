bff3d5a902251d02f13f22067be831adc217143b
coskun <coskunntirass@gmail.com>
2025-01-26 21:56:11 +0300

    product-detail-page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,8 @@
-    export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || " Mbf Store";
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || " Mbf Store";
 
-    export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "Mbf Store";
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "Mbf Store";
 
-    export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Mbf Store";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Mbf Store";
 
-        
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
