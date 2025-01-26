2e714f603e0c4abc902924f8c96818d967ccf1a2
coskun <coskunntirass@gmail.com>
2025-01-26 14:19:37 +0300

    carousel

--- lib/data.ts
+++ lib/data.ts
@@ -33,6 +33,39 @@ const data = {
       href: "/page/help",
     },
   ],
+
+
+  carousels: [
+    {
+      title: 'Popüler 3D Maketler',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner3.jpg',
+      url: '/search?category=Shoes',
+      isPublished: true,
+    },
+    {
+      title: 'Hayallerinizdeki Görselin 3D Maketini Alın',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner1.jpg',
+      url: '/search?category=T-Shirts',
+      isPublished: true,
+    },
+    {
+      title: '3D Maketlerimiz',
+      buttonCaption: 'See More',
+      image: '/images/banner2.jpg',
+      url: '/search?category=Wrist Watches',
+      isPublished: true,
+    },
+  ],
+
+
+
+
+
+
+
+
 };
 
 export default data;
