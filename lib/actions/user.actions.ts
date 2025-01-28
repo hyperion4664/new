984b24369a45410b06d3bf59598ce6def3e14fbb
coskun <coskunntirass@gmail.com>
2025-01-28 12:17:24 +0300

    sign-in-google

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,22 +1,21 @@
-'use server'
+"use server";
 
-import bcrypt from 'bcryptjs'
-import { signIn, signOut } from '@/auth'
-import { IUserSignIn, IUserSignUp } from '@/types'
-import { UserSignUpSchema } from '../validator'
-import { connectToDatabase } from '../db'
-import User from '../db/models/user.model'
-import { formatError } from '../utils'
-import { redirect } from 'next/navigation'
+import bcrypt from "bcryptjs";
+import { signIn, signOut } from "@/auth";
+import { IUserSignIn, IUserSignUp } from "@/types";
+import { UserSignUpSchema } from "../validator";
+import { connectToDatabase } from "../db";
+import User from "../db/models/user.model";
+import { formatError } from "../utils";
+import { redirect } from "next/navigation";
 
 export async function signInWithCredentials(user: IUserSignIn) {
-  return await signIn('credentials', { ...user, redirect: false })
+  return await signIn("credentials", { ...user, redirect: false });
 }
 export const SignOut = async () => {
-  const redirectTo = await signOut({ redirect: false })
-  redirect(redirectTo.redirect)
-}
-
+  const redirectTo = await signOut({ redirect: false });
+  redirect(redirectTo.redirect);
+};
 
 // CREATE
 export async function registerUser(userSignUp: IUserSignUp) {
@@ -26,15 +25,19 @@ export async function registerUser(userSignUp: IUserSignUp) {
       email: userSignUp.email,
       password: userSignUp.password,
       confirmPassword: userSignUp.confirmPassword,
-    })
+    });
 
-    await connectToDatabase()
+    await connectToDatabase();
     await User.create({
       ...user,
       password: await bcrypt.hash(user.password, 5),
-    })
-    return { success: true, message: 'User created successfully' }
+    });
+    return { success: true, message: "User created successfully" };
   } catch (error) {
-    return { success: false, error: formatError(error) }
+    return { success: false, error: formatError(error) };
   }
 }
+
+export const SignInWithGoogle = async () => {
+  await signIn("google");
+};
