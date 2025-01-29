c305661485cb36c0897d742978646aa9b818208b
coskun <coskunntirass@gmail.com>
2025-01-29 12:07:19 +0300

    menu-header-sign-in

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,21 +1,21 @@
-
 import React from "react";
 import CartButton from "./cart-button";
 import UserButton from './user-button'
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
-
-
-const menu = () => {
+const Menu = () => {
   return (
     <div className="flex justify-end items-center h-full">
       <nav className="flex items-center gap-4">
-    
+        <Link href="/sign-in" passHref>
+          <Button variant="outline">Sign In</Button>
+        </Link>
         <UserButton />
-
         <CartButton />
       </nav>
     </div>
   );
 };
 
-export default menu;
+export default Menu;
